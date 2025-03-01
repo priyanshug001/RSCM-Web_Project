69d4dc80ffefa768e31577a94549050ad050ad07
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-01 19:46:58 +0530

    Update ServerConfig.java

--- src/main/java/com/RSCM/config/ServerConfig.java
+++ src/main/java/com/RSCM/config/ServerConfig.java
@@ -1,6 +1,8 @@
 package com.RSCM.config;
 
+import org.apache.catalina.Context;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
