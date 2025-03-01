09865267905efeaedb89d082801ee190cecf6a5e
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-01 20:23:54 +0530

    Update ServerConfig.java

--- src/main/java/com/RSCM/config/ServerConfig.java
+++ src/main/java/com/RSCM/config/ServerConfig.java
@@ -1,6 +1,5 @@
 package com.RSCM.config;
 
-import java.io.File;
 import org.apache.catalina.Context;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -14,7 +13,6 @@ public class ServerConfig {
     public TomcatServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.addContextCustomizers((Context context) -> context.setAllowCasualMultipartParsing(true)); // File upload support
-        factory.setDocumentRoot(new File("src/main/webapp"));
         return factory;
     }
 
